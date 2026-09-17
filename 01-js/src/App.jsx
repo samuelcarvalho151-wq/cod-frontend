85a237e19b46eb6c5afe6eddb6fc9913ec8f5c7c
SAMUEL DOS SANTOS CARVALHO <samuel_carvalho151@estudante.sesisenai.org.br>
2026-09-17 17:06:45 -0300

    acabando o nivel b

--- 01-js/src/App.jsx
+++ 01-js/src/App.jsx
@@ -92,6 +92,89 @@ function calcularpontos(){
 
 
  }
+ function transpo(){
+ let peso, distancia, volume
+ peso = Number(prompt('Peso do Frete(KG): '))
+ distancia = Number(prompt('Distancia do Frete(KM): '))
+ volume = Number(prompt('Volume do Frete(M3): '))
+
+ let frete = 15+(2*peso)+(0.05*distancia)+(10*volume)
+
+ alert('O valor do Frete é R$:' + frete.toFixed(2))
+
+
+ }
+ function dondon(){
+ let bruto, premiacoes, presentes, comissoes
+ bruto = Number(prompt('Valor Bruto: R$: '))
+ premiacoes = Number(prompt('Valor das Premiações: R$: '))
+ presentes = Number(prompt('Valor dos Presentes: R$: '))
+ comissoes = Number(prompt('Valor das Comissões: R$: '))
+
+ let lucro = bruto - premiacoes - presentes - comissoes
+
+ alert('O seu Lucro: R$: ' + lucro )
+
+
+
+ }
+ function cap(){
+ let gasto, fatuin, fatuitens
+ gasto = Number(prompt('Quanto foi gasto em suprimentos e mercadorias para operar seu navio: '))
+ fatuin = Number(prompt('Quanto foi o faturamento em venda de ingressos: '))
+ fatuitens = Number(prompt('Quando foi o faturamento em venda de ítens: '))
+
+ let lucroreais = fatuin + fatuitens - gasto
+ alert('Lucro obtido em reais: ' + lucroreais)
+ let porcentagem = (lucroreais / gasto) * 100
+ alert('Lucro percentual: ' + porcentagem.toFixed(2) + '%')
+
+
+ }
+ function sarru(){
+   let shows, precouni, bombas, custoTotal
+   shows = Number(prompt('quantos shows foram realizados: '))
+   precouni = Number(prompt('qual o preço unitario do ingresso: '))
+   bombas = Number(prompt('quantas bombas foram vendidas: '))
+   
+   bombas = shows * 7;
+   custoTotal = bombas * precouni;
+
+   alert('O faturamento total foi: R$: ' + custoTotal)
+
+
+ }
+ function man(){
+  let  salario, moradia, agua, Luz, internet, gasolina, streamings, telefone, outros
+  salario = Number(prompt('Qual o valor do seu salário: R$: '))
+  moradia = Number(prompt('Qual o valor do seu gasto com moradia: R$: '))
+  agua = Number(prompt('Qual o valor do seu gasto com água: R$: '))
+  Luz = Number(prompt('Qual o valor do seu gasto com luz: R$: '))
+  internet = Number(prompt('Qual o valor do seu gasto com internet: R$: '))
+  gasolina = Number(prompt('Qual o valor do seu gasto com gasolina: R$: '))
+  streamings = Number(prompt('Qual o valor do seu gasto com streamings: R$: '))
+  telefone = Number(prompt('Qual o valor do seu gasto com telefone: R$: '))
+  outros = Number(prompt('Qual o valor do seu gasto com outros: R$: '))
+
+  let sobranadaprobeta = salario - moradia - agua - Luz - internet - gasolina - streamings - telefone - outros
+  alert('O valor que sobrou na sua probeta: R$: ' + sobranadaprobeta.toFixed(2) )
+
+
+
+ }
+ function brique(){
+    let precoCusto = Number(prompt('Quanto seu Romero pagou na obra de arte?'));
+
+    let lucro = precoCusto * 2;
+    let precoVenda = precoCusto + lucro; 
+
+    alert('Preço de venda recomendado: R$ ' + precoVenda.toFixed(2));
+
+
+
+
+
+ }
   return (
     <div className="cont-app">
     <h1>JavaScript no React</h1>
@@ -111,7 +194,14 @@ function calcularpontos(){
     <button onClick={candev}>Monika</button>
     <hr />
     <h2>Exercicios supimpas nivel (b)</h2>
-    
+
+    <button onClick={transpo}>Telles Transportes</button>
+    <button onClick={dondon}>Dona Bete</button>
+    <button onClick={cap}>Capitão Ganso</button>
+    <button onClick={sarru}>Sarumano</button>
+    <button onClick={man}>Mano Juca</button>
+    <button onClick={brique}>Romero Brique</button>
+
     </div>
     
 
